944117cf5cd70d76dc38a72cef44b1c717a96c3c
Anuta Bledea <anabledea@hotmail.es>
2026-07-10 09:33:59 +0200

    simular error

--- Mundial2026API.Tests/SeleccionControllerTests.cs
+++ Mundial2026API.Tests/SeleccionControllerTests.cs
@@ -34,7 +34,7 @@ public class SeleccionControllerTests
     {
         // Act
         var seleccion =
-            await _client.GetFromJsonAsync<Seleccion>("/api/selecciones/1");
+            await _client.GetFromJsonAsync<Seleccion>("/api/selecciones/2");
 
         // Assert
         seleccion.Should().NotBeNull();
